a66121598e011522f6133a3176d09e0c26d488b1
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2013-01-03 02:12:12 +0900

    some perf. fix for code completion. Ongoing parameter completion work.
    (not sure what it'd be like though...)

--- TypeScriptBinding/Languages/Gui/TypeScriptCompletionTextEditorExtension.cs
+++ TypeScriptBinding/Languages/Gui/TypeScriptCompletionTextEditorExtension.cs
@@ -63,11 +63,21 @@ namespace MonoDevelop.TypeScriptBinding.Languages.Gui
 			    ||*/ ap.IsArgumentOfClassConstructor () || ap.IsArgumentOfFunction ()) {
 				var doc = Document.ToString ();
 				int idx = doc.LastIndexOf ('(', startOffset);
-				Console.WriteLine ("  idx: " + idx);
+				LoggingService.LogDebug ("  idx: " + idx);
 				if (idx >= 0)
 					return doc.Substring (idx, startOffset - idx).Count (c => c == ',');
 			}
 			return base.GetCurrentParameterIndex (startOffset);
+		}
+		
+		public override bool KeyPress (Gdk.Key key, char keyChar, Gdk.ModifierType modifier)
+		{
+			bool result = base.KeyPress (key, keyChar, modifier);
+			
+			if (EnableParameterInsight && (keyChar == ',' || keyChar == ')') && CanRunParameterCompletionCommand ())
+				base.RunParameterCompletionCommand ();
+			
+			return result;
 		}
 
 		TypeScriptLS shimHost {
@@ -80,21 +90,73 @@ namespace MonoDevelop.TypeScriptBinding.Languages.Gui
 
 		public override ICompletionDataList HandleCodeCompletion (CodeCompletionContext completionContext, char completionChar, ref int triggerWordLength)
 		{
+			if (!EnableCodeCompletion)
+				return null;
+			if (!EnableAutoCodeCompletion && char.IsLetter (completionChar))
+				return null;
+			
+			try {
+				if (char.IsLetterOrDigit (completionChar) || completionChar == '_') {
+					if (completionContext.TriggerOffset > 1 && char.IsLetterOrDigit (document.Editor.GetCharAt (completionContext.TriggerOffset - 2)))
+						return null;
+					triggerWordLength = 1;
+				}
+				return InternalHandleCodeCompletion (completionContext, completionChar, false, ref triggerWordLength);
+			} catch (Exception e) {
+				LoggingService.LogError ("Unexpected code completion exception." + Environment.NewLine + 
+				                         "FileName: " + Document.FileName + Environment.NewLine + 
+				                         "Position: line=" + completionContext.TriggerLine + " col=" + completionContext.TriggerLineOffset + Environment.NewLine + 
+				                         "Line text: " + Document.Editor.GetLineText (completionContext.TriggerLine), 
+				                         e);
+				return null;
+			}
+		}
+
+		ICompletionDataList InternalHandleCodeCompletion (CodeCompletionContext completionContext, char completionChar, bool ctrlSpace, ref int triggerWordLength)
+		{
 			if (completionContext != null) {
 				string file = service.GetFilePath (Document.FileName);
 				shimHost.UpdateScript (file, Editor.Text);
 				var ret = new CompletionDataList ();
-				var ast = ls.GetScriptAST (file);
 				var list = ls.GetCompletionsAtPosition (file, (int) completionContext.TriggerOffset, true);
-				foreach (var entry in list.Entries)
-					ret.Add (new CompletionData (entry.Name, MonoDevelop.Core.IconId.Null, entry.Name + " : " + entry.Type));
+				foreach (var entry in list.Entries) {
+					IconId icon = IconId.Null;
+					// FIXME: fill icons
+					/*
+					if (entry.Kind == ScriptElementKind.MemberFunctionElement
+					    || entry.Kind == ScriptElementKind.FunctionElement)
+						icon = methodIcon;
+					else if (entry.Kind == ScriptElementKind.MemberVariableElement
+					    || entry.Kind == ScriptElementKind.VariableElement)
+						icon = fieldIcon;
+					*/
+					ret.Add (new CompletionData (entry.Name, icon, string.Format ("({0}) {1} : {2}", entry.Kind, entry.Name, entry.Type)));
+				}
 				return ret;
 			}
 			return base.HandleCodeCompletion (completionContext, completionChar, ref triggerWordLength);
 		}
 		
+		internal Mono.TextEditor.TextEditorData TextEditorData {
+			get {
+				var doc = Document;
+				if (doc == null)
+					return null;
+				return doc.Editor;
+			}
+		}
+
+		public override ICompletionDataList CodeCompletionCommand (CodeCompletionContext completionContext)
+		{
+			int triggerWordLength = 0;
+			char ch = completionContext.TriggerOffset > 0 ? TextEditorData.GetCharAt (completionContext.TriggerOffset - 1) : '\0';
+			return InternalHandleCodeCompletion (completionContext, ch, true, ref triggerWordLength);
+		}
+
 		public override IParameterDataProvider HandleParameterCompletion (CodeCompletionContext completionContext, char completionChar)
 		{
+			LoggingService.LogDebug ("HandleParameterCompletion: ({0},{1}) {2}",
+			                   completionContext.TriggerLine, completionContext.TriggerLineOffset, completionContext.TriggerOffset);
 			return base.HandleParameterCompletion (completionContext, completionChar);
 		}
 
